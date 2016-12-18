7e9bd1b814556915d2c3b7892d91911b8ce163c9
Sumanth Shankar <sumanth.shankar@hebbalelabs.com>
2016-12-18 03:23:57 +0100

    adds details necessary for dev environment

--- webpack-config/named-module-plugin.js
+++ webpack-config/named-module-plugin.js
@@ -1,3 +1,3 @@
 const webpack = require( 'webpack' );
 
-module.exports = new webpack.NamedModulesPlugin()
+module.exports = new webpack.NamedModulesPlugin();
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require( 'path' );
 
 // loaders
 const react_loader = require( './webpack-config/react-loader' );
-const webpack = require( 'webpack' );
 
 // plugins
 const html_webpack_plugin = require( './webpack-config/html-webpack-plugin' );
@@ -11,36 +10,67 @@ const copy_webpack_plugin = require( './webpack-config/copy-webpack-plugin' );
 const commons_chunk_plugin = require( './webpack-config/commons-chunk-plugin' );
 const hot_module_plugin = require( './webpack-config/hot-module-plugin' );
 const named_module_plugin = require( './webpack-config/named-module-plugin' );
+const process_env_plugin = require( './webpack-config/process-env-plugin' );
+
+// dev server
 const dev_server = require( './webpack-config/dev-server' );
 
-// entry file array
-const entry = [];
+// define the entry file array of the webpack config
+const entry = [
+	'./src/main.js',
+	'./src/vendors'
+];
 
-module.exports = {
-	entry: [
+// define output file object of the webpakc config
+const output = {
+	filename: '[name].js',
+	path: path.join( __dirname, 'dist' ),
+	publicPath: '/'
+};
+
+// define the modules object of the webpack
+const modules = {
+	loaders: [ react_loader ]
+};
+
+// define the plugins array of the webpack
+const plugins = [
+	html_webpack_plugin,
+	copy_webpack_plugin,
+	commons_chunk_plugin,
+	process_env_plugin
+];
+
+// define the source maps to use based on the Environment
+let source_maps = '';
+
+const env = process.env.NODE_ENV;
+// Depending on the environment the following
+// things will be added to the webpack config objects
+switch ( env ) {
+case 'dev':
+	// add support for hot reloading feature. the 3 files mentioned below are required
+	// for hot reloading react application
+	entry.unshift(
 		'react-hot-loader/patch',
 		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./src/main.js',
-		'./src/vendors'
-	],
-	output: {
-		filename: '[name].js',
-		path: path.join( __dirname, 'dist' ),
-		publicPath: '/'
-	},
-	module: {
-		loaders: [ react_loader ]
-	},
-	plugins: [
-		html_webpack_plugin,
-		copy_webpack_plugin,
-		commons_chunk_plugin,
+		'webpack/hot/only-dev-server' );
+	plugins.push(
 		hot_module_plugin,
-		named_module_plugin
-	],
+		named_module_plugin );
+	source_maps = 'inline-source-map';
+	break;
+default:
+
+}
+
+module.exports = {
+	entry: entry,
+	output: output,
+	module: modules,
+	plugins: plugins,
 	devServer: dev_server,
-	devtool: 'inline-source-map',
+	devtool: source_maps,
 	performance: {
 		hints: false
 	}
